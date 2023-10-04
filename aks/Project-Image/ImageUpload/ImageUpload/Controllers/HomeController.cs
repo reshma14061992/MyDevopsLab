ec3e00532e8114303c549ef711c17cf6d9914b9f
Reshma <reshma.rahulraj@gmail.com>
2023-10-05 01:47:43 +0530

    I am pushing aks files

---
+++ aks/Project-Image/ImageUpload/ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using ImageUpload.Models;
+using ImageUpload.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IOptions<MyCustomPath> _path;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment, IOptions<MyCustomPath> path)
+        {
+            _logger = logger;
+            webHostEnvironment = hostEnvironment;
+            _path = path;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(UploadsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = UploadedFile(model);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        private string UploadedFile(UploadsViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProfileImage != null)
+            {
+               var path= _path.Value.cutomepath;
+                //string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProfileImage.FileName;
+                string filePath = Path.Combine(path, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProfileImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
